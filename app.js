73ead40c723db3caba664973cd945b499ba8f7b6
israel386 <israel_delarosa@knights.ucf.edu>
2022-06-14 22:56:03 -0400

    created page-template.js and updated app.js to generate the HTML file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = (profileDataArr) => {
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
