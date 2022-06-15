d54635ecc4f83e8fd82620c02ddb84c302c0e86a
israel386 <israel_delarosa@knights.ucf.edu>
2022-06-14 22:57:23 -0400

    n/a

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template');
-
 const profileDataArgs = process.argv.slice(2);
-
 const [name, github] = profileDataArgs;
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
