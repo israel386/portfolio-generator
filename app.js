9e099def6dc97a0ec5edba8f10ca6cef930a7f2d
israel386 <israel_delarosa@knights.ucf.edu>
2022-06-14 23:29:53 -0400

    created a .gitignore

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const generatePage = require('./src/page-template');
 const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
